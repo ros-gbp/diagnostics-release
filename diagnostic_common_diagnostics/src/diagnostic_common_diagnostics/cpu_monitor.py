7509d389c3aa934218c3ceca0f918174a3dc6bd6
trainman419 <legotown@aol.com>
2017-07-15 16:58:40 -0700

    Rebase from 'upstream'

--- diagnostic_common_diagnostics/src/diagnostic_common_diagnostics/cpu_monitor.py
+++ diagnostic_common_diagnostics/src/diagnostic_common_diagnostics/cpu_monitor.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
+#
 # Software License Agreement (BSD License)
 #
-# Copyright (c) 2009, Willow Garage, Inc.
+# Copyright (c) 2017, TNO IVS, Helmond, Netherlands
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -14,7 +15,7 @@
 #    copyright notice, this list of conditions and the following
 #    disclaimer in the documentation and/or other materials provided
 #    with the distribution.
-#  * Neither the name of the Willow Garage nor the names of its
+#  * Neither the name of the TNO IVS nor the names of its
 #    contributors may be used to endorse or promote products derived
 #    from this software without specific prior written permission.
 #
@@ -30,40 +31,55 @@
 # LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
 # ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
-#
 
-##\author Kevin Watts
-
-##\brief Publishes messages for aggregator testing of expected items.
-
-PKG = 'diagnostic_aggregator'
-
-import roslib; roslib.load_manifest(PKG)
-
+# \author Rein Appeldoorn
 
 import rospy
-from time import sleep
+from diagnostic_updater import DiagnosticTask, Updater
+from diagnostic_msgs.msg import DiagnosticStatus
 
-from diagnostic_msgs.msg import DiagnosticArray, DiagnosticStatus
+import psutil
+import socket
+
+
+class CpuTask(DiagnosticTask):
+    def __init__(self, warning_percentage):
+        DiagnosticTask.__init__(self, "CPU Information")
+        self._warning_percentage = int(warning_percentage)
+
+    def run(self, stat):
+        cpu_percentages = psutil.cpu_percent(percpu=True)
+        cpu_average = sum(cpu_percentages) / len(cpu_percentages)
+
+        stat.add("CPU Load Average", cpu_average)
+
+        warn = False
+        for idx, val in enumerate(cpu_percentages):
+            stat.add("CPU {} Load".format(idx), "{}".format(val))
+            if val > self._warning_percentage:
+                warn = True
+
+        if warn:
+            stat.summary(DiagnosticStatus.WARN, "At least one CPU exceeds %d percent" % self._warning_percentage)
+        else:
+            stat.summary(DiagnosticStatus.OK, "CPU Average %.1f percent" % cpu_average)
+
+        return stat
+
+
+def main():
+    hostname = socket.gethostname()
+    rospy.init_node('cpu_monitor_%s' % hostname.replace("-", "_"))
+
+    updater = Updater()
+    updater.setHardwareID(hostname)
+    updater.add(CpuTask(rospy.get_param("~warning_percentage", 90)))
+
+    rate = rospy.Rate(rospy.get_param("~rate", 1))
+    while not rospy.is_shutdown():
+        rate.sleep()
+        updater.update()
+
 
 if __name__ == '__main__':
-    rospy.init_node('diag_pub')
-    pub = rospy.Publisher('/diagnostics', DiagnosticArray, queue_size=10)
-    
-    start_time = rospy.get_time()
-    
-    while not rospy.is_shutdown():
-        array = DiagnosticArray()
-        array.header.stamp = rospy.get_rostime()
-        array.status = [
-            # GenericAnalyzer my_path
-            DiagnosticStatus(0, 'multi', 'OK', '', []),
-            DiagnosticStatus(1, 'Something', 'OK', '', []),
-            DiagnosticStatus(2, 'Something Else', 'OK', '', []),
-            
-            # OtherAnalyzer for Other
-            DiagnosticStatus(2, 'other2', 'OK', '', []),
-            DiagnosticStatus(0, 'other3', 'OK', '', [])]
-        
-        pub.publish(array)
-        sleep(1)
+    main()
